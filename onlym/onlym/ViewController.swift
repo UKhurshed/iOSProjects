5ec0def34b1c97ad1d7c15c4e0801a76a1c72f31
Ukhurshed <khurshedumarov777@gmail.com>
2021-10-13 11:46:03 +0500

    added in module onlym project

---
+++ onlym/onlym/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  onlym
+//
+//  Created by Khurshed Umarov on 13.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
