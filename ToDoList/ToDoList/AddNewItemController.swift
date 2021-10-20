9d57a913ffa2c539cc4fa2ea80ddb227485357f7
Ukhurshed <khurshedumarov777@gmail.com>
2021-10-20 15:16:46 +0500

    Onlym Test Problem added

---
+++ ToDoList/ToDoList/AddNewItemController.swift
@@ -0,0 +1,56 @@
+//
+//  AddNewItemController.swift
+//  ToDoList
+//
+//  Created by Khurshed Umarov on 16.10.2021.
+//
+
+import UIKit
+
+class AddNewItemController: UIViewController {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    private var models = [ToDoListItem]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func backtToMain(){
+        let storeboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storeboard.instantiateViewController(withIdentifier: "view_controller")
+        navigationController?.pushViewController(viewController, animated: true)
+
+    }
+    
+    @IBAction func didTapAdd(){
+        let alert = UIAlertController(title: "New Item", message: "Enter new item", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(UIAlertAction(title: "Submit", style: .cancel, handler: { [weak self] _ in
+            guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else{
+                return
+            }
+            self?.createItem(name: text)
+        }))
+        present(alert, animated: true)
+    }
+    
+    func createItem(name: String){
+        let items = ["item1", "item2", "item3"]
+        for item in items{
+            let newItem = ToDoListItem(context: context)
+            newItem.name = item
+            newItem.data = Date()
+        }
+        do{
+            try context.save()
+        }catch{
+            
+        }
+    }
+    
+}
--- ToDoList/ToDoList/ViewController.swift
+++ ToDoList/ToDoList/ViewController.swift
@@ -56,6 +56,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+        print("Model:",isEmptyModel())
+        
+        print("viewDidLoad")
         title = "CoreData To Do List"
         view.addSubview(tableView)
         getAll()
@@ -63,26 +67,71 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate  = self
         tableView.frame = view.bounds
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTapNext))
     }
     
-    @objc private func didTapAdd(){
-        let alert = UIAlertController(title: "New Item", message: "Enter new item", preferredStyle: .alert)
-        alert.addTextField(configurationHandler: nil)
-        alert.addAction(UIAlertAction(title: "Submit", style: .cancel, handler: { [weak self] _ in
-            guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else{
-                return
+    func isEmptyModel() -> Bool{
+        do{
+            models = try context.fetch(ToDoListItem.fetchRequest())
+            if(models.count < 1){
+                print("model is empty")
+                return false
+            }else{
+                return true
             }
-            self?.createItem(name: text)
-        }))
-        present(alert, animated: true)
+        }catch{
+            return false
+        }
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getAll()
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
+    
+    
+    
+    @objc private func didTapNext(){
+        let storeboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storeboard.instantiateViewController(withIdentifier: "new_item_vc")
+        navigationController?.pushViewController(viewController, animated: true)
+    
+    }
+    
+//    @objc private func didTapAdd(){
+//        let alert = UIAlertController(title: "New Item", message: "Enter new item", preferredStyle: .alert)
+//        alert.addTextField(configurationHandler: nil)
+//        alert.addAction(UIAlertAction(title: "Submit", style: .cancel, handler: { [weak self] _ in
+//            guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else{
+//                return
+//            }
+//            self?.createItem(name: text)
+//        }))
+//        present(alert, animated: true)
+//    }
     
     //Core Data
     
     func getAll(){
         do {
             models = try context.fetch(ToDoListItem.fetchRequest())
+            print("Model: \(models.count)")
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
