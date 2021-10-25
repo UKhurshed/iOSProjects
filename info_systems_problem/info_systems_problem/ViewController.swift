5c64877e5f5fd989fa4ea26b4d1d28bdf56447c5
Ukhurshed <khurshedumarov777@gmail.com>
2021-10-25 18:31:26 +0500

    Added info_systems_problem submodule

---
+++ info_systems_problem/info_systems_problem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  info_systems_problem
+//
+//  Created by Khurshed Umarov on 25.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
