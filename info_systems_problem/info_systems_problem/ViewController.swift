0066d3032b7b04027d7c14fefc28e7ceb15e245e
Ukhurshed <khurshedumarov777@gmail.com>
2021-10-26 12:59:35 +0500

    First Feature is completed

--- info_systems_problem/info_systems_problem/ViewController.swift
+++ info_systems_problem/info_systems_problem/ViewController.swift
@@ -5,15 +5,80 @@
 //  Created by Khurshed Umarov on 25.10.2021.
 //
 
+//API_KEY = fc9c93a0395d4d82ac5d3c736f184179
+
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var tableView: UITableView!
+    
+    private var viewModel = [NewsTableViewCellViewModel]()
+    private var articles = [Articles]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        title = "News"
+        tableView.register(NewsTableViewCell.self, forCellReuseIdentifier: NewsTableViewCell.identifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        fetchTopStories()
+        
     }
+    
+    private func fetchTopStories(){
+        APICaller.shared.getTopStories{ [weak self] result in
+            switch result {
+            case .success(let articles):
+                self?.articles = articles
+                self?.viewModel = articles.compactMap({
+                    NewsTableViewCellViewModel(title: $0.title, subtitle: $0.description ?? "No description", imageUrl: URL(string: $0.urlToImage ?? ""))
+                })
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
 
+extension ViewController: UITableViewDelegate{
+    
+}
 
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: NewsTableViewCell.identifier, for: indexPath) as? NewsTableViewCell else {
+            fatalError()
+        }
+        cell.configure(with: viewModel[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let article = articles[indexPath.row]
+        
+        guard let url = URL(string: article.url ?? "") else{
+            return
+        }
+        
+        let vc = SFSafariViewController(url: url)
+        present(vc, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150 
+    }
 }
 
